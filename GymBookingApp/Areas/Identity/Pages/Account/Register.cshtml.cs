5464fc3ad59c9dd007d64cff65ebf069e3df38a7
Xiahui Liu <coffeeliuxiahui@gmail.com>
2023-12-07 15:36:24 +0100

    Update login with FirstName and LastName

--- GymBookingApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ GymBookingApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -80,6 +80,13 @@ namespace GymBookingApp.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
 
+            [Required]
+            [Display(Name = "First name")]
+            public string FirstName { get; set; }
+            [Required]
+            [Display(Name = "Last name")]
+            public string LastName { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
--- GymBookingApp/Models/ApplicationUser.cs
+++ GymBookingApp/Models/ApplicationUser.cs
@@ -1,15 +1,16 @@
-﻿
-
-using Microsoft.AspNetCore.Identity;
-
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace GymBookingApp.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string FullName => FirstName + " " + LastName;
+
+        public DateTime TimeOfRegistration { get; set; }
+
         // Navigation property
         public ICollection<ApplicationUserGymClass>? GymClasses { get; set; }
     }
-
-        
 }

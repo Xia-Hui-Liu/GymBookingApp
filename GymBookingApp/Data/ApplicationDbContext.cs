651cdd74f937ffe266ba806264cd1d168bb8cfcb
Xiahui Liu <coffeeliuxiahui@gmail.com>
2023-12-07 09:08:06 +0100

    Add applicationUser to DbContext

--- GymBookingApp/Data/ApplicationDbContext.cs
+++ GymBookingApp/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GymBookingApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
